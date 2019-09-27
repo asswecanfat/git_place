6d486dc32dbb8c9165be17bc7cd04b1f9b44b622
asswecan <1024847824@qq.com>
2019-09-27 15:31:42 +0800

    v1.0

--- learn/trace_func.py
+++ learn/trace_func.py
@@ -1,6 +1,8 @@
+import functools  # 用于查看被装饰的函数的元数据和文档数据
 
 
 def trace_func(func):
+    @functools.wraps(func)  # 此处能将被装饰函数的元数据复制到装饰器闭包中，方便调试被装饰函数
     def _fuck(*args, **kwargs):
         real_data = None
         print(f'开始跟踪：{func.__name__}()函数，'
@@ -15,9 +17,11 @@ def trace_func(func):
     return _fuck
 
 
-''''@trace_func
+'''@trace_func
 def ttee(a: list):
+    """asd"""
     return [i for i in a if i != None]
 
 if __name__ == '__main__':
-    ttee([1], [1])'''
+    print(ttee.__name__)
+    print(ttee.__doc__)'''
