b120d43e35ff0d02dcb17d695e3c29f58cd1789e
asswecan <1024847824@qq.com>
2019-09-27 15:24:51 +0800

    v1.0

--- learn/trace_func.py
+++ learn/trace_func.py
@@ -2,9 +2,22 @@
 
 def trace_func(func):
     def _fuck(*args, **kwargs):
+        real_data = None
         print(f'开始跟踪：{func.__name__}()函数，'
               f'输入参数为：{args}, {kwargs}')
-        real_data = func(*args, **kwargs)
-        print(f'{func.__name__}()函数返回了：{real_data}')
+        try:
+            real_data = func(*args, **kwargs)
+        except TypeError:
+            print(f'{func.__name__}()函数的参数输入有误！！')
+        else:
+            print(f'{func.__name__}()函数返回了：{real_data}')
         return real_data
     return _fuck
+
+
+''''@trace_func
+def ttee(a: list):
+    return [i for i in a if i != None]
+
+if __name__ == '__main__':
+    ttee([1], [1])'''
