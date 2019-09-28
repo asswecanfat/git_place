417423d9695ee0dfcd5a8b2c47e176bfc57e6059
asswecan <1024847824@qq.com>
2019-09-28 09:55:59 +0800

    v1.0

---
+++ learn/cls_func__sta_func__func.py
@@ -0,0 +1,25 @@
+
+
+class HotDog(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'HotDog({self.name!r})'
+
+    @classmethod
+    def name_fuck(cls):
+        return cls('fuck')
+
+    @classmethod
+    def name_dick(cls):
+        return cls('dick')
+
+
+if __name__ == '__main__':
+    h = HotDog('ass')
+    print(h)
+    print(h.name_dick())
+    print(h.name_fuck())
+    print(HotDog.name_fuck())
+    print(HotDog.name_dick())
